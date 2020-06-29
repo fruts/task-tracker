d63a557cbcd32aff57263509b54e5fd0055a961f
unknown <fruts1997@gmail.com>
2020-06-30 01:12:34 +0300

    first commit

---
+++ src/test/java/com/tsyhankov/tasktracker/TaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsyhankov.tasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
